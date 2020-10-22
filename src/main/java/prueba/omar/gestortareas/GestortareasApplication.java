df8c2f6ed549dce7677396a87ddba12d0057a648
Omar El Assaad <omarelasaad.zenta@latam.com>
2020-10-22 15:27:11 -0300

    first commit

---
+++ src/main/java/prueba/omar/gestortareas/GestortareasApplication.java
@@ -0,0 +1,13 @@
+package prueba.omar.gestortareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestortareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestortareasApplication.class, args);
+	}
+
+}
